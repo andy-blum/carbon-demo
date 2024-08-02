d4b2ebc8b5b535022b98bc5eabd690d0f2f6dd40
Andy Blum <andy.blum.01@gmail.com>
2024-08-02 11:58:08 -0400

    Add vite config for multiple inputs

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+// vite.config.js
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [],
+  build: {
+    rollupOptions: {
+      input: {
+        index: fileURLToPath(new URL('./index.html', import.meta.url)),
+        cinemagraph: fileURLToPath(new URL('./cinemagraph.html', import.meta.url)),
+        leadspace: fileURLToPath(new URL('./colorized-leadspace.html', import.meta.url)),
+        statistics: fileURLToPath(new URL('./statistics.html', import.meta.url)),
+        pictograms: fileURLToPath(new URL('./pictograms.html', import.meta.url)),
+      },
+    },
+  },
+})
