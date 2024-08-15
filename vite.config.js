14982afdc3c42f33ac72e3520b09bc8ad2e5a0b5
Andy Blum <andy.blum.01@gmail.com>
2024-08-15 14:32:08 -0400

    argh

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import find from 'find';
 
+const htmlFiles = find.fileSync('./')
+  .filter(file => (
+    file.endsWith('.html') &&
+    !file.startsWith('node_modules/') &&
+    !file.startsWith('dist/')
+  ))
+
+
 export default defineConfig({
   plugins: [],
   build: {
