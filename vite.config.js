da90e7f64a08ab23627d52ed5b885c65900ae9ea
Andy Blum <andy.blum.01@gmail.com>
2024-08-15 14:30:19 -0400

    make vite build automatic

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,13 @@
 // vite.config.js
 import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
+import find from 'find';
 
 export default defineConfig({
   plugins: [],
   build: {
     rollupOptions: {
-      input: {
-        index: fileURLToPath(new URL('./index.html', import.meta.url)),
-        cinemagraph: fileURLToPath(new URL('./cinemagraph.html', import.meta.url)),
-        leadspace: fileURLToPath(new URL('./colorized-leadspace.html', import.meta.url)),
-        statistics: fileURLToPath(new URL('./statistics.html', import.meta.url)),
-        pictograms: fileURLToPath(new URL('./pictograms.html', import.meta.url)),
-        tabs: fileURLToPath(new URL('./tabs.html', import.meta.url)),
-        tabs: fileURLToPath(new URL('./tabs-2.html', import.meta.url))
-      },
+      input: htmlFiles,
     },
   },
 })
